8fe63b9c7db5ada1db15d0c3da1dd902b9dbf185
kevinamatte <81264481+kevinamatte@users.noreply.github.com>
2023-05-17 10:33:13 -0600

    Add files via upload

---
+++ Get-SCCMService.ps1
@@ -0,0 +1,20 @@
+﻿$servers = Get-Content C:\users\amkmatte\desktop\servers.txt
+$tbl = @()
+$cred = Get-Credential
+
+$servers | %{
+    $sesh = New-PSSession -ComputerName $_ -Credential $cred
+    If (!$sesh){
+        $i = [pscustomobject] @{"Server"=$_;"Service"="Could not connect"}
+        $tbl += $i}
+    Else {
+        $srv = Invoke-Command -Session $sesh -ScriptBlock {Get-Service ccmexec -ErrorAction SilentlyContinue}
+        If (!$srv){
+            $i = [pscustomobject] @{"Server"=$_;"Service"="Service Not Found"}
+            $tbl += $i}
+        Else {
+            $i = [pscustomobject] @{"Server"=$_;"Service"="Installed"}
+            $tbl += $i}}
+        }
+$tbl | Out-GridView
+    
